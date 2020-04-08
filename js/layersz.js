19bad14679fcfabe0109009ae2239b4a7cbd38af
db325 <31334165+db325@users.noreply.github.com>
2020-04-08 16:57:36 -0700

    latest commit

--- js/layersz.js
+++ js/layersz.js
@@ -1,32 +1,118 @@
-var sources =[
-    "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Observations/radar_base_reflectivity/MapServer",
-    "https://idpgis.ncep.noaa.gov/arcgis/rest/services/radar/radar_base_reflectivity_time/ImageServer",
-    "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/watch_warn_adv/MapServer",
-    "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Observations/NOHRSC_Snow_Analysis/MapServer",
-    "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/NHC_E_Pac_trop_cyclones/MapServer",
-    "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Climate_Outlooks/cpc_weather_hazards/MapServer",
-    "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/NDFD_temp/MapServer",
-    "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/natl_fcst_wx_chart/MapServer",
-    "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/wpc_precip_hazards/MapServer",
-    "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/wpc_prob_winter_precip/MapServer",
-    "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/SPC_wx_outlks/MapServer",
-    "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Observations/ahps_riv_gauges/MapServer",
-    "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/sig_riv_fld_outlk/MapServer",
-    "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/wpc_qpf/MapServer",
-    "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/rfc_hourly_qpe/MapServer",
-    "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/rfc_dly_qpe/MapServer",
-    "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/rfc_gridded_ffg/MapServer",
-    "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/aprfc_RiverBreakupStatus/MapServer"
-]
 
 
+// Default data objects
+let radarBaseTime={
+    url:"https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Observations/radar_base_reflectivity/MapServer",
+    name:"Radar Base (Time)"
 
+}
+let radarBaseImage={
+    url: "https://idpgis.ncep.noaa.gov/arcgis/rest/services/radar/radar_base_reflectivity_time/ImageServer",
 
-const Base={
-    "name":"",
-    "url":""
+    name:"Radar Base (Image)"
+
+}
+let forcastGuidance={
+    url:"https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/watch_warn_adv/MapServer",
+
+    name:"Forceast Guidance Warnings"
+
+}
+let radarsnowAnalysis={
+    url: "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Observations/NOHRSC_Snow_Analysis/MapServer",
+
+    name:"Snow Analysis"
+
+}
+let tropicalCyclones={
+    url:"https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/NHC_E_Pac_trop_cyclones/MapServer",
+
+    name:"Tropical Cyclones"
+
+}
+let cpcWeatherHaz={
+    url:    "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Climate_Outlooks/cpc_weather_hazards/MapServer",
+
+    name:"CPC Weather Hazards"
+
+}
+let ndfdTemp={
+    url:"https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/NDFD_temp/MapServer", 
+    name:"NDFD Temp"
+
+}
+let nationalForecast={
+    url: "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/natl_fcst_wx_chart/MapServer",
+    
+    name:"National Forecast"
+
+}
+let WpcPrecip={
+    url: "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/wpc_precip_hazards/MapServer",
+    
+    name:"WPC Precip Hazards"
+
+}
+let wpc_prob_winter_precip={
+    url: "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/wpc_prob_winter_precip/MapServer",
+    name:"WPC Winter Precip Probability"
+
+}
+let spcWxOutlooks={
+    url:  "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/SPC_wx_outlks/MapServer",
+
+    name:"SPC Wx Outlooks"
+
+}
+let ahps_riv_gauges={
+    url: "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Observations/ahps_riv_gauges/MapServer",
+
+    name:"ACHPS River Guages"
+
+}
+let sig_riv_fld_outlk={
+    url:    "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/sig_riv_fld_outlk/MapServer",
+
+    name:"SIG River Flood Outlook"
+
+}
+let wpc_qpf={
+    url:  "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/wpc_qpf/MapServer",
+    
+    name:"WPC QPF"
+
+}
+let rfc_hourly_qpe={
+    url:    "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/rfc_hourly_qpe/MapServer",
+
+    name:"RFC Hourly QPE"
+
+}
+let rfc_dly_qpe={
+    url:"https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/rfc_dly_qpe/MapServer",
+
+    name:"RFC Daily QPE"
+
+}
+let rfc_gridded_ffg={
+    url: "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/rfc_gridded_ffg/MapServer",
+
+    name:"RFC Gridded FFG"
+
+}
+let aprfc_RiverBreakupStatus={
+    url:"https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/aprfc_RiverBreakupStatus/MapServer",
+
+    name:"APRFC River Breakup Status"
+
 }
 
+//Default DataLayer Array
+
+var DATALAYER =[radarBaseTime,radarBaseImage,forcastGuidance,radarsnowAnalysis,tropicalCyclones,cpcWeatherHaz,ndfdTemp,nationalForecast,wpc_prob_winter_precip,spcWxOutlooks,ahps_riv_gauges,sig_riv_fld_outlk,wpc_qpf,rfc_hourly_qpe,rfc_dly_qpe,rfc_gridded_ffg,aprfc_RiverBreakupStatus]
+  
+
+//Default Base Layers
 let  WorldPhysical={
     url:'https://server.arcgisonline.com/ArcGIS/rest/services/World_Physical_Map/MapServer/tile/{z}/{y}/{x}',
     name:"World Physical"
@@ -40,18 +126,28 @@ let dark={
     name:"Dark Carto"
 }
 
-const BASES=[WorldPhysical,OceanBase,dark]
+let WorldGeo= {
+    url:"https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+    
+    name:"World Geo"
+ 
+}
+let OSM = new ol.layer.Tile({
+    source:new ol.source.OSM()
+    ,
+    name:"OSM"
+  })
+  
+const BASES=[WorldPhysical,OceanBase,dark,WorldGeo]
 
 
-
-
+//Query string to add to url
 const fmtString="/?f=json"
 
 
 
-
-const dataL=document.getElementById('data-layers')
-
+   
+   
 
 function createBase(url,name){
     let base= new ol.layer.Tile({
@@ -75,5 +171,59 @@ function createBaseLayersGroup(baseObjectArray){
   return baseLayerArray
 }
 
+// function to create a single dataLayer
+function createDataLayer(url,name){
+    let dataLayer= new ol.layer.Tile({
+       source:new ol.source.TileArcGISRest({
+           url:url
+       }),
+      
+       visible:false
+       ,
+       crossOrigin:"anonymous",
+       cors:true,
+       name:name,
+       
+       
+   })
+  
+   return dataLayer
+  }
+  
 
+  let DLG= new ol.layer.Group({
+    layers:createDataLayersGroup(DATALAYER)
+    })
+    function createDataLayersGroup(dataObjectArray){
+        let dataLayerArray=[]
+      
+      for(let o=0;o<DATALAYER.length;o++){
+        let l= createDataLayer(DATALAYER[o].url,DATALAYER[o].name)
+            dataLayerArray.push(l)
+           console.log(l)
+      
+      //d.push(l)
+      }
+      
+        return dataLayerArray
+      }
+      
 
+      let dlyr= document.getElementById('data-layers')
+      let dlg= DLG.getLayers().getArray()
+      DATALAYER.forEach(d=>{
+          let b= document.createElement('button')
+          b.innerText=d.name
+          b.value=d.name
+          let v=b.value
+          b.className="hvr-radial-out"
+          b.addEventListener('click',()=>{
+   dlg.forEach((l,i)=>{
+       if(v===l.get('name')){
+           l.setVisible(true)
+       }
+   })
+          })
+          dlyr.appendChild(b)
+      })
+    
--- js/main.js
+++ js/main.js
@@ -11,16 +11,7 @@ let Style= ol.style;
 let createXYZ = ol.tilegrid;
 
 
-
-
-
-
-
-// Get default data
-
-sources.forEach((index)=>{
-  getInfo(index)
-})
+let testArray=[]
 
 
 
@@ -28,97 +19,6 @@ sources.forEach((index)=>{
 
 
 
-
-
-var serviceUrl =     `https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Forecasts_Guidance_Warnings/watch_warn_adv/MapServer/`;
-var layer = '1';
-
-var esrijsonFormat = new EsriJSON();
-
-var styleCache = {
-    "Frost Advisory": new ol.style.Style({
-    fill: new Style.Fill({
-      color: 'rgba(225, 225, 225, 255)'
-    }),
-    stroke: new ol.style.Stroke({
-      color: 'rgba(0, 0, 0, 255)',
-      width: 0.4
-    })
-  }),
-  'Flood Warning': new ol.style.Style({
-    fill: new ol.style.Fill({
-      color: 'rgba(255, 0, 0, 255)'
-    }),
-    stroke: new ol.style.Stroke({
-      color: 'rgba(110, 110, 110, 255)',
-      width: 0.4
-    })
-  }),
-  'Wind Advisory': new ol.style.Style({
-    fill: new ol.style.Fill({
-      color: 'rgba(56, 168, 0, 255)'
-    }),
-    stroke: new ol.style.Stroke({
-      color: 'rgba(110, 110, 110, 255)',
-      width: 0
-    })
-  }),
-  'OILGAS': new ol.style.Style({
-    fill: new ol.style.Fill({
-      color: 'rgba(168, 112, 0, 255)'
-    }),
-    stroke: new ol.style.Stroke({
-      color: 'rgba(110, 110, 110, 255)',
-      width: 0.4
-    })
-  })
-};
-
-var vectorSource = new VectorSource({
-  loader: function(extent, resolution, projection) {
-    var url = serviceUrl + layer + '/query/?f=json&' +
-        'returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry=' +
-        encodeURIComponent('{"xmin":' + extent[0] + ',"ymin":' +
-            extent[1] + ',"xmax":' + extent[2] + ',"ymax":' + extent[3] +
-            ',"spatialReference":{"wkid":102100}}') +
-        '&geometryType=esriGeometryEnvelope&inSR=102100&outFields=*' +
-        '&outSR=102100';
-    $.ajax({url: url, dataType: 'jsonp', success: function(response) {
-      if (response.error) {
-        console.log(response.error.message + '\n' +
-            response.error.details.join('\n'));
-      } else {
-        // dataProjection will be read from document
-        var features = esrijsonFormat.readFeatures(response, {
-          featureProjection: projection
-        });
-        console.log(features[0],"  features  ")
-        if (features.length > 0) {
-          vectorSource.addFeatures(features);
-        }
-      }
-    }});
-  },
-  strategy:new tilesrat.tile ( new createXYZ.createXYZ({
-    tileSize: 512
-  }))
-});
-
-var vector = new Vector.Vector({
-  source: vectorSource,
-  style: function(feature) {
-    var classify = feature.get(`prod_type`);
-    return styleCache[classify];
-  }
-});
-
-var raster = new ol.layer.Tile({
-  source: new XYZ({
-    attributions: 'Tiles © <a href="https://services.arcgisonline.com/ArcGIS/' +
-        'rest/services/World_Topo_Map/MapServer">ArcGIS</a>',
-    url:BASES[2].url
-  })
-});
 
 
 var LG= new ol.layer.Group({
@@ -132,45 +32,18 @@ var LG= new ol.layer.Group({
 
 
 
-let OSM = new ol.layer.Tile({
-  source:new ol.source.OSM()
-  ,
-  name:"OSM"
-})
-
-let WorldGeo= new ol.layer.Tile({
-  source:new XYZ({
-    url:"https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
-    crossOrigin:"anonymous",
-    cors:true,
-    zIndex:15
-  }),
-  name:"World Geo"
-})
-
 let l= LG.getLayers()
-l.push(WorldGeo)
-console.log(l)
 
-const Dlg= new ol.layer.Group({
-  layers:[]//simpleDataLayers(sources)})
-})
 
-  let dlgarr=Dlg.getLayers().getArray()
 
-console.log(dlgarr)
 var map = new Map({
-  layers: [OSM,LG,Dlg],
+  layers: [OSM,LG,DLG],
   target: document.getElementById('map'),
   view: new View({
     center: fromLonLat.fromLonLat([-97.6114, 38.8403]),
     zoom: 3.6,
   })
 });
-//map.addLayer(DATAGROUP)
-
-
-console.log(map.getLayers().getArray()[2])
 
 
 
@@ -209,8 +82,6 @@ map.on('click', function(evt) {
 
 
 
-
-
 const buttons= document.getElementsByClassName('base-btn')
 let ls= LG.getLayers().getArray()
 for (let butt of buttons){
@@ -243,63 +114,5 @@ for (let butt of buttons){
 
 }
 
-
-
-function createDataLayer(url,name,attr){
-  let dataLayer= new ol.layer.Tile({
-     source:new ol.source.TileArcGISRest({
-         url:url
-     }),
-    
-     visible:true
-     ,
-     crossOrigin:"anonymous",
-     cors:true,
-     name:name,
-     attributions:attr
-     
- })
-
- return dataLayer
-}
-
-function simpleDataLayers(array){
-  let ar=[]
-
-for(let o=0;o<array.length;o++){
-  let l= createDataLayer(array[o])
-      ar.push(l)
-      console.log(l)
-}
-
-
-
-  // array.forEach(element => {
-  //     let l= createDataLayer(element)
-  //     ar.push(l)
-  //     console.log(l)
-  // });
-  return ar
-}
-
-
-
-
-function getInfo(url){
-let rn={}
-  fetch(url+fmtString)
-  .then((resp) => resp.json()) // Transform the data into json
-  .then(function(data) {
-    // Create and append the li's to the ul
-    rn= {
-      url:url,
-      name:data.mapName,
-      attr:data.copyrightText
-    }
-    dlgarr.push(createDataLayer(rn.url,rn.name,rn.attr))
-    testArray.push(createDataLayer(rn.url,rn.name,rn.attr))
-    })
-    
-}
 
 
